3f38e9212194842abd78b9ab6b77eee96702d604
Abhishek H <abh1sh3kh@gmail.com>
2022-01-21 20:46:24 +0530

    first commit

---
+++ tk-dice-roller/main.py
@@ -0,0 +1,20 @@
+import random
+from tkinter import *
+
+root = Tk()
+root.title("Dice Roller")
+root.geometry("700x450")
+
+l1 = Label(root, font=("times", 200))
+
+
+def roll():
+    number = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+    l1.config(text=f'{random.choice(number)}{random.choice(number)}')
+    l1.pack()
+
+
+b1 = Button(root, text="Roll Dice", command=roll)
+b1.place(x=330, y=0)
+
+root.mainloop()
